e8f9c3946f7789abf56003fd3c52fda53e01c8be
Samuel <kazegolala@gmail.com>
2024-03-20 23:42:54 +0800

    r2

--- word_time/world_time/lib/pages/choose_location.dart
+++ word_time/world_time/lib/pages/choose_location.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:world_time/services/world_time.dart';
 
 
 class ChooseLocation extends StatefulWidget {
@@ -8,9 +9,16 @@ class ChooseLocation extends StatefulWidget {
 
 class _ChooseLocationState extends State<ChooseLocation> {
 
-  int counter = 0;
-
-  
+  List<WorldTime> locations = [
+    WorldTime(url: 'Europe/London', location: 'London', flag: 'uk.png'),
+    WorldTime(url: 'Europe/Berlin', location: 'Athens', flag: 'greece.png'),
+    WorldTime(url: 'Africa/Cairo', location: 'Cairo', flag: 'egypt.png'),
+    WorldTime(url: 'Africa/Nairobi', location: 'Nairobi', flag: 'kenya.png'),
+    WorldTime(url: 'America/Chicago', location: 'Chicago', flag: 'usa.png'),
+    WorldTime(url: 'America/New_York', location: 'New York', flag: 'usa.png'),
+    WorldTime(url: 'Asia/Seoul', location: 'Seoul', flag: 'south_korea.png'),
+    WorldTime(url: 'Asia/Jakarta', location: 'Jakarta', flag: 'indonesia.png'),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -23,13 +31,27 @@ class _ChooseLocationState extends State<ChooseLocation> {
         centerTitle: true,
         elevation: 0,
       ),
-      body: ElevatedButton(
-        onPressed: (){
-          setState(() {
-            counter += 1;
-          });
-        }, 
-        child: Text('counter is $counter')),
+      // body: ElevatedButton(
+      //   onPressed: (){
+      //     setState(() {
+      //       counter += 1;
+      //     });
+      //   }, 
+      //   child: Text('counter is $counter')),
+      body: ListView.builder(
+        itemCount: locations.length,
+        itemBuilder: (context, index){
+          return Card(
+            child: ListTile(
+              onTap: () {},
+              title: Text(locations[index].location),
+              leading: CircleAvatar(
+                backgroundImage: AssetImage('assets/${locations[index].flag}'),
+                ),
+          ),
+          );
+        }
+        ),
     );
   }
 }
